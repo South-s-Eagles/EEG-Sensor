f8b1e6fd58c98b10ee042a0747f2792c5dbf2ec8
IvanCCO <ivanmedeiros0903@outlook.com>
2024-03-16 20:09:05 +0000

    very difficult

--- simulator/waves.py
+++ simulator/waves.py
@@ -23,20 +23,14 @@ class Wave:
         self.min_freq = min_freq
         self.max_freq = max_freq
         self.variation = random.uniform(min_freq, max_freq)
-        self.actual = round(random.uniform(min_freq, max_freq), 2)
+        self.actual = round(min_freq, 2)
         self.curve = random.choice(list(Sign))
         self.flow = flow
 
     def to_string(self):
         return f"name: {self.name}\nmin_freq: {self.min_freq}\nmax_freq: {self.max_freq}\nvariation: {self.variation}\nactual: {self.actual}\ncurve: {self.curve.value}"
 
-    def is_max_out_of_bound(self) -> bool:
-        return self.actual >= self.max_freq
-
-    def is_min_out_of_bound(self) -> bool:
-        return self.actual <= self.min_freq
-
-    def generate_delta_wave(self):
+    def generate_wave(self):
         self.curve = self.curve.choose_sign(self.flow)
 
         variation_ratio = random.uniform(0.001, 0.003)
@@ -52,30 +46,21 @@ class Wave:
         else:
             self.actual = round(min(self.max_freq, self.actual + variation_amount), 2)
 
-    def plot_frequency_graph(self, frequencies, seconds):
-        plt.plot(frequencies)
-        plt.xlabel(f"{seconds} segundo(s)")
-        plt.ylabel("Frequência (em Hz)")
-        plt.title(f"Ondas {self.name}")
-        plt.show()
+    def is_max_out_of_bound(self) -> bool:
+        return self.actual >= self.max_freq
+
+    def is_min_out_of_bound(self) -> bool:
+        return self.actual <= self.min_freq
+
+
+def plot_frequency_graph(frequencies, title):
+    plt.plot(frequencies)
+    plt.xlabel(f"Tempo em minutos")
+    plt.ylabel("Frequência (em Hz)")
+    plt.title(title)
+    plt.show()
 
 
 if __name__ == "__main__":
-    wave = Wave("Delta", 0.5, 4, 0.9)
-    inicio = time.time()
-    frequency = []
-
+    duration = 10000
     time_variation = 1 / 1000
-    seconds = 5
-
-    while True:
-        tempo_atual = time.time()
-
-        if tempo_atual - inicio > seconds:
-            break
-
-        wave.generate_delta_wave()
-        frequency.append(wave.actual)
-        time.sleep(time_variation)
-
-    wave.plot_frequency_graph(frequency, seconds)
