9913e2b4f7c5815728697c921e193ca820bf483c
IvanCCO <ivanmedeiros0903@outlook.com>
2024-03-29 16:17:58 +0000

    Http request para enviar para o broker

--- simulator-v2/main.go
+++ simulator-v2/main.go
@@ -1,41 +1,50 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
-	"rsc.io/quote"
+	_ "rsc.io/quote"
 )
 
-// NOTE:
-// Dispositivo em si do eeg
+// Dispositivo em si do EEG
 // Ele é a "caixa preta" que vai ter os
-// sensores. Tipo o produto final,
-// que vai ter a "inteligência", ele
-// vai ser o dispositivo IOT de fato,
+// sensores, vai ser o dispositivo IOT de fato,
 // que vai conseguir enviar os dados tratados
 // conseguir ter a inteligência de tratar os dados
 // recebidos pelo sensor e guardar para enviar com
 // calma para o broker (IOT HUB)
-type Dispositivo struct{}
+type Dispositivo struct {
+	Sensor     Sensor  // Sensores que existem no Dispositivo
+	Frequencia float64 // Valor em hz da Frequencia da corrente elétrica
+	Amplitude  float64 // Valor da Amplitude da onda da corrente elétrica
+}
 
-type Sensor struct{}
+// TODO: Transforma os dados recebidos em hz
+func (d Dispositivo) turnToHz(uV ...float64) {
+}
+
+// Envia os dados em heartz recebidos para o broker
+func (d Dispositivo) sendToBroker() {
+	http.Get("https://6606dbacbe53febb857ec850.mockapi.io/go/pessoa")
+}
+
+// O sensor Eletrodo é apenas o sensor,
+// ele tem a função apenas de capturar
+// os dados (corrente elétrica) em
+// uV (microvolts) e enviar para o Dispositivo
+type Sensor struct {
+	Valor float64 // Valor em microvolt da corrente elétrica
+}
+
+func (s Sensor) generateValue() int {
+	return 0
+}
 
 func main() {
-	fmt.Println(quote.Go())
-}
-
-func soma(x int, y int) (int, error) {
-	res := x + y
-
-	if res > 10 {
-		return 0, errors.New("total maior que 10")
+	sensor := Sensor{
+		Valor: 10.0,
 	}
 
-	return res, nil
-}
-
-func callExternalApiTest() (any, error) {
-	return http.Get("https://6606dbacbe53febb857ec850.mockapi.io/go/pessoa")
+	fmt.Println(sensor.generateValue())
 }
